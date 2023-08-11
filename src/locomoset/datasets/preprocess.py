ad6a78029188699599bc2e99a6b237e24261fd42
Jack Roberts <jroberts@turing.ac.uk>
2023-08-11 15:42:58 +0100

    :memo: add more detail about expected data format to docstring

--- src/locomoset/datasets/preprocess.py
+++ src/locomoset/datasets/preprocess.py
@@ -10,15 +10,26 @@ def preprocess(dataset: Dataset, processor: BaseImageProcessor) -> Dataset:
     compatibility with a model.
 
     Args:
-        dataset: HuggingFace image dataset to process.
+        dataset: HuggingFace image dataset to process. Each samples in the dataset is
+            expected to have a key 'image' containing a PIL image that will be converted
+            to RGB format and run through the processor. The processed result is saved
+            under the key "pixel_values" and the "image" key is removed.
         processor: HuggingFace trained pre-processor to use.
 
     Returns:
         Processed dataset with feature 'pixel_values' instead of 'image'.
     """
 
-    def proc_sample(sample):
-        """Process one sample: Convert image to RGB and run through processor"""
+    def proc_sample(sample: dict) -> dict:
+        """Process one sample: Convert image to RGB and run through processor
+
+        Args:
+            sample: Sample from dataset with key 'image' containing a PIL image.
+
+        Returns:
+            Processed sample with feature 'pixel_values' (a tensor of pixel values
+            representing the processed image) and the 'image' feature deleted.
+        """
         sample["pixel_values"] = processor(sample["image"].convert("RGB"))[
             "pixel_values"
         ][0]
