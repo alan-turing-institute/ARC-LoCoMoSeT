c74c84cd21f165b88a2a37d4a019621896598d23
Jack Roberts <jroberts@turing.ac.uk>
2023-08-14 13:08:53 +0100

    :memo: note on large tolerance used in test

--- tests/test_renggli.py
+++ tests/test_renggli.py
@@ -19,6 +19,10 @@ def test_renggli_random_features(dummy_features_random, dummy_labels, dummy_n_cl
     Test that the Renggli score is around 1/n_classes (i.e. same as random) if the
     features are just random noise.
     """
+    # As there are a lot of sources of randomness in the Renggli score (train/test split
+    # and model fit), it takes a large number of samples for the score to converge to
+    # exactly 1/n_classes. We use a large rel tolerance here rather than using a much
+    # larger dummy dataset.
     assert renggli(dummy_features_random, dummy_labels) == pytest.approx(
-        1 / dummy_n_classes, rel=0.3
+        1 / dummy_n_classes, rel=0.1
     )
