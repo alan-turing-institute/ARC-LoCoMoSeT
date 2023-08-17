5b2996f00eef1a97096276fbf8a8bfbb971a28fc
Jack Roberts <jroberts@turing.ac.uk>
2023-08-17 16:39:29 +0100

    :sparkles: WIP ImageFeaturesPipeline class

---
+++ src/locomoset/models/pipeline.py
@@ -0,0 +1,73 @@
+import torch
+from transformers import Pipeline
+from transformers.modeling_outputs import ImageClassifierOutput
+
+
+class ImageFeaturesPipeline(Pipeline):
+    """
+    A custom pipeline for returning the features of an image from a model with its
+    classification head removed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        for expected_arg in ["model", "image_processor"]:
+            if expected_arg not in kwargs:
+                raise ValueError(
+                    f"{expected_arg} must be passed to ImageFeaturesPipeline"
+                )
+        super().__init__(*args, **kwargs)
+
+    def _sanitize_parameters(self, **pipeline_parameters: dict) -> (dict, dict, dict):
+        """Not used here but required for inheriting from the Pipeline class. Returns
+        parameters to pass to the `preprocess`, `forward` and `postprocess` methods"""
+        return {}, {}, {}
+
+    def preprocess(
+        self, input_: dict, **preprocess_parameters: dict
+    ) -> dict[str, torch.tensor]:
+        """Preprocess a single image.
+
+        Args:
+            input_: Single sample from an image dataset, a dict with the key 'image'
+                containing a PIL image.
+            preprocess_parameters: Unused here but required for compatibility with the
+                base Pipeline class.
+
+        Returns:
+            Processed sample, a dict with the key 'pixel_values' containing a tensor.
+        """
+        return self.image_processor(input_["image"].convert("RGB"), return_tensors="pt")
+
+    def _forward(
+        self, model_inputs: dict[str, torch.tensor], **forward_parameters: dict
+    ) -> ImageClassifierOutput:
+        """Pass a batch of processed images to the headless model.
+
+        Args:
+            model_inputs: A batch of processed images, a dict with the key
+                'pixel_values' containing a tensor with shape
+                (batch_size, n_channels, image_size, image_size).
+            forward_parameters: Unused here but required for compatibility with the
+                base Pipeline class.
+
+        Returns:
+            Output of the model, in this including the key `logits` which contains the
+                features of each image in the batch.
+        """
+        return self.model(model_inputs["pixel_values"])
+
+    def postprocess(
+        self, model_outputs: ImageClassifierOutput, **postprocess_parameters: dict
+    ) -> torch.tensor:
+        """Extract features from the model output.
+
+        Args:
+            model_outputs: Output of the model, in this including the key `logits` which
+                contains the features for one image.
+            postprocess_parameters: Unused here but required for compatibility with the
+                base Pipeline class.
+
+        Returns:
+            Tensor containing features for one image.
+        """
+        return model_outputs["logits"]
