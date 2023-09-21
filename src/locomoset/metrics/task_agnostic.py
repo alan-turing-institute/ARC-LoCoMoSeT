899931b278fd80b80d1b5f09a8f10f37186cdbda
eddableheath <edable-heath@turing.ac.uk>
2023-09-21 15:28:07 +0100

    Update src/locomoset/metrics/task_agnostic.py
    Co-authored-by: Jack Roberts <jroberts@turing.ac.uk>

--- src/locomoset/metrics/task_agnostic.py
+++ src/locomoset/metrics/task_agnostic.py
@@ -1,14 +1,6 @@
 """
 Functions for the task-agnostic metrics. In particular the number of parameters of a
 model as a metric.
-
-This also includes functions for reweighting a set of metric values by the inclusion of
-a task agnostic metric as a proxy for 'capactity to learn', as introduced in the
-Pairwise Annotation Representation Comparison (PARC) metric for transferability paper:
-
-Bolya, Daniel, Rohit Mittapalli, and Judy Hoffman. "Scalable diverse model selection
-for accessible transfer learning." Advances in Neural Information Processing Systems
-34 (2021): 19301-19312.
 """
 
 from transformers.modeling_utils import PreTrainedModel
