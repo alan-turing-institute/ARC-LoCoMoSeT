5f6c4c1187a63977901a33cfc144c6ec3ccb21a1
eddableheath <edable-heath@turing.ac.uk>
2023-09-21 15:31:01 +0100

    :test_tube: updated task agnostic test to consider dummy_model_head not dummy_model_classifier

--- tests/test_task_agnostic.py
+++ tests/test_task_agnostic.py
@@ -5,8 +5,8 @@ Test functions for the task_agonstic.py metrics.
 from locomoset.metrics.task_agnostic import num_params_metric
 
 
-def test_num_params_metric(dummy_model_classifier):
+def test_num_params_metric(dummy_model_head):
     """
     Test that the number of parameters metric returns the correct value.
     """
-    assert num_params_metric(dummy_model_classifier) == 1698
+    assert num_params_metric(dummy_model_head) == 1686
