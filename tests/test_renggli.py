d5471cc09d30fbca4737284218dc5be2d4dab364
Jack Roberts <jroberts@turing.ac.uk>
2023-09-21 11:20:49 +0100

    fix random seed in renggli tests

--- tests/test_renggli.py
+++ tests/test_renggli.py
@@ -6,15 +6,17 @@ import pytest
 from locomoset.metrics.renggli import renggli
 
 
-def test_renggli_perfect_features(dummy_features_perfect, dummy_labels):
+def test_renggli_perfect_features(dummy_features_perfect, dummy_labels, test_seed):
     """
     Test that the Renggli score is 1 if the features give perfect information
     about the labels.
     """
-    assert renggli(dummy_features_perfect, dummy_labels) == 1
+    assert renggli(dummy_features_perfect, dummy_labels, random_state=test_seed) == 1
 
 
-def test_renggli_random_features(dummy_features_random, dummy_labels, dummy_n_classes):
+def test_renggli_random_features(
+    dummy_features_random, dummy_labels, dummy_n_classes, test_seed
+):
     """
     Test that the Renggli score is around 1/n_classes (i.e. same as random) if the
     features are just random noise.
@@ -23,6 +25,6 @@ def test_renggli_random_features(dummy_features_random, dummy_labels, dummy_n_cl
     # and model fit), it takes a large number of samples for the score to converge to
     # exactly 1/n_classes. We use a large rel tolerance here rather than using a much
     # larger dummy dataset.
-    assert renggli(dummy_features_random, dummy_labels) == pytest.approx(
-        1 / dummy_n_classes, rel=0.3
-    )
+    assert renggli(
+        dummy_features_random, dummy_labels, random_state=test_seed
+    ) == pytest.approx(1 / dummy_n_classes, rel=0.2)
