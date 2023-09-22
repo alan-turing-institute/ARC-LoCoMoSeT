f4e83fabe3526b4f944b31245bb3a7d2db41a836
eddableheath <edable-heath@turing.ac.uk>
2023-09-22 15:46:22 +0100

    :thermometer: added aggregation of metric for metric scores

--- src/locomoset/metrics/library.py
+++ src/locomoset/metrics/library.py
@@ -1,7 +1,18 @@
+from locomoset.metrics.metrics_for_metrics import (
+    kendall_tau_correlation,
+    pearsons_correlation,
+    spearmans_rank_correlation,
+)
 from locomoset.metrics.parc import parc
 from locomoset.metrics.renggli import renggli
 
 METRIC_FUNCTIONS = {
     "parc": parc,
     "renggli": renggli,
+}
+
+METRIC_FOR_METRIC_FUNCTIONS = {
+    "spearmans": spearmans_rank_correlation,
+    "pearsons": pearsons_correlation,
+    "kendall_tau": kendall_tau_correlation,
 }
