298af05b607b5ea5e7ea6430ac44ff20a0f472ef
eddableheath <edable-heath@turing.ac.uk>
2023-09-25 16:10:33 +0100

    Update library.py

--- src/locomoset/metrics/library.py
+++ src/locomoset/metrics/library.py
@@ -3,4 +3,4 @@ from locomoset.metrics.nce import nce
 from locomoset.metrics.parc import parc
 from locomoset.metrics.renggli import renggli
 
-METRIC_FUNCTIONS = {"parc": parc, "renggli": renggli, "LogME": logme, "NCE": nce}
+METRIC_FUNCTIONS = {"parc": parc, "renggli": renggli, "LogME": logme, "NCE": nce, "n_pars": num_params_metric}
