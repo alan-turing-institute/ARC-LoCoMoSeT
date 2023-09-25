c72e0cc487fec61ed267ad4b8ffd4a667656f086
eddableheath <edable-heath@turing.ac.uk>
2023-09-25 16:05:38 +0100

    :test_tube: updated test_logme.py

--- tests/test_logme.py
+++ tests/test_logme.py
@@ -16,40 +16,63 @@ def test_logme_n_samples_catch(dummy_features_perfect, dummy_labels):
         logme(dummy_features_perfect, dummy_labels)
 
 
-def test_logme_perfect_features(dummy_features_perfect, dummy_labels):
+def test_logme_perfect_features(
+    dummy_features_perfect, dummy_labels, logme_minimum_datatpoints_required
+):
     """Test that LogME returns a value greater than zero for perfect features, with less
-    than zero only seen for random features"""
+    than zero only seen for random features.
+
+    Empirically at least 3500 datapoints are required for a representative score."""
     features = np.concatenate(
         [
             np.asarray(dummy_features_perfect)
-            for _ in range((3500 // dummy_features_perfect.shape[0] + 1))
+            for _ in range(
+                (
+                    logme_minimum_datatpoints_required
+                    // dummy_features_perfect.shape[0]
+                    + 1
+                )
+            )
         ],
         axis=0,
     )
     labels = np.concatenate(
         [
             dummy_labels.reshape(1, -1)
-            for _ in range((3500 // dummy_labels.shape[0] + 1))
+            for _ in range(
+                (logme_minimum_datatpoints_required // dummy_labels.shape[0] + 1)
+            )
         ],
         axis=1,
     )
     assert logme(features, labels) > 0
 
 
-def test_logme_random_features(dummy_features_random, dummy_labels):
+def test_logme_random_features(
+    dummy_features_random, dummy_labels, logme_minimum_datatpoints_required
+):
     """Test that LogME returns a value less than zero for random features, with greater
-    than zero only seen for good features"""
+    than zero only seen for good features
+
+    Empirically at least 3500 datapoints are required for a representative score."""
     features = np.concatenate(
         [
             np.asarray(dummy_features_random)
-            for _ in range((3500 // dummy_features_random.shape[0] + 1))
+            for _ in range(
+                (
+                    logme_minimum_datatpoints_required // dummy_features_random.shape[0]
+                    + 1
+                )
+            )
         ],
         axis=0,
     )
     labels = np.concatenate(
         [
             dummy_labels.reshape(1, -1)
-            for _ in range((3500 // dummy_labels.shape[0] + 1))
+            for _ in range(
+                (logme_minimum_datatpoints_required // dummy_labels.shape[0] + 1)
+            )
         ],
         axis=1,
     )
