0a6e31818235835d705e8b4cdd3b9398f7ed06d4
eddableheath <edable-heath@turing.ac.uk>
2023-09-25 15:25:45 +0100

    split aggregate metrics function out into own script

--- src/locomoset/metrics/metrics_for_metrics.py
+++ src/locomoset/metrics/metrics_for_metrics.py
@@ -10,11 +10,9 @@ We also include the an aggregate (mean) correlation over datasets considered for
 these.
 """
 
-# import numpy as np
+
 from numpy.typing import ArrayLike
 from scipy.stats import kendalltau, pearsonr, spearmanr
-
-# from locomoset.metrics.library import METRIC_FOR_METRIC_FUNCTIONS
 
 
 def spearmans_rank_correlation(metric_scores: ArrayLike, validation_scores: ArrayLike):
@@ -50,39 +48,3 @@ def kendall_tau_correlation(metric_scores: ArrayLike, validation_scores: ArrayLi
         validation_scores: validation accuracy scores for each model, (s, ) for s models
     """
     return kendalltau(metric_scores, validation_scores)[0] * 100
-
-
-# def aggregate_metric_scores(
-#     metric_scores: ArrayLike,
-#     validation_scores: ArrayLike,
-#     metric_for_metrics: str,
-#     by_dataset: bool = True,
-# ):
-#     """Aggregate the metric scores by taking the mean over either varying datasets:
-
-#     (1/|T|) * sum_T corr([S], [V])
-
-#     for set of task datsets T, and models S, or over models:
-
-#     (1/|S|) * sum_M corr([T], [V])
-
-#     Args:
-#         metric_scores: metric scores for each model for all datasets considered, (T,
-# S)
-#                         for T datasets and S models
-#         validation_scores: validation accuracy scores for each model, (S, ) for S
-# models
-#         metric_for_metrics: which of the above metric for metrics to use
-#         by_dataset: controls if the aggregation is over varying datasets or varying
-#                     models
-#     """
-#     if not by_dataset:
-#         metric_scores = metric_scores.T
-#     return np.sum(
-#         [
-#             METRIC_FOR_METRIC_FUNCTIONS[metric_for_metrics](
-#                 metric_vals, validation_scores
-#             )
-#             for metric_vals in metric_scores
-#         ]
-#     ) * (1 / metric_scores.shape[0])
