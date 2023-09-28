9e74b273b9ca8f228f79657c440279375404bb43
eddableheath <edable-heath@turing.ac.uk>
2023-09-28 16:23:57 +0100

    updated logme tests

--- tests/test_logme.py
+++ tests/test_logme.py
@@ -21,18 +21,4 @@ def test_logme_perfect_features(dummy_features_perfect, dummy_labels, logme_n_sa
 def test_logme_random_features(dummy_features_random, dummy_labels, logme_n_samples):
     """Test that LogME returns a value less than zero for random features, with greater
     than zero only seen for good features"""
-    # features = np.concatenate(
-    #     [
-    #         np.asarray(dummy_features_random)
-    #         for _ in range((logme_n_samples // dummy_features_random.shape[0] + 1))
-    #     ],
-    #     axis=0,
-    # )
-    # labels = np.concatenate(
-    #     [
-    #         dummy_labels.reshape(1, -1)
-    #         for _ in range((logme_n_samples // dummy_labels.shape[0] + 1))
-    #     ],
-    #     axis=1,
-    # )
     assert logme(dummy_features_random, dummy_labels) < 0
