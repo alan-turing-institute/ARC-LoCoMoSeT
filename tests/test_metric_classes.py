5159b0ca998066e5d1135b697297a8f1c7b8f462
eddableheath <edable-heath@turing.ac.uk>
2023-10-04 17:00:24 +0100

    :test_tube: fixed failing test

--- tests/test_metric_classes.py
+++ tests/test_metric_classes.py
@@ -12,77 +12,96 @@ from locomoset.metrics.metric_classes import (
 )
 
 
-def test_n_pars_class(dummy_model_head):
+def test_n_pars_class(dummy_model_head, dummy_features_perfect, dummy_labels):
     """Test the task agonistic metric class"""
     metric = NumParsMetric()
     assert metric.metric_name == "n_pars"
     assert metric.inference_type is None
     assert metric.dataset_dependent is False
-    assert metric.fit_metric(dummy_model_head) == 1686
+    assert (
+        metric.fit_metric(dummy_model_head, dummy_features_perfect, dummy_labels)
+        == 1686
+    )
 
 
 def test_renggli_class_perfect_features(
-    dummy_features_perfect, dummy_labels, test_seed
+    dummy_model_head, dummy_features_perfect, dummy_labels, test_seed
 ):
     """Test the Renggli metric class for perfect features"""
-    metric = RenggliMetric()
+    metric = RenggliMetric(**{"random_state": test_seed})
     assert metric.metric_name == "renggli"
     assert metric.inference_type == "features"
     assert metric.dataset_dependent is True
     assert (
-        metric.fit_metric(dummy_features_perfect, dummy_labels, random_state=test_seed)
+        metric.fit_metric(
+            dummy_model_head,
+            dummy_features_perfect,
+            dummy_labels,
+        )
         == 1
     )
 
 
 def test_renggli_class_random_features(
-    dummy_features_random, dummy_labels, dummy_n_classes, test_seed
+    dummy_model_head, dummy_features_random, dummy_labels, dummy_n_classes, test_seed
 ):
     """Test the Renggli metric class for random features"""
-    metric = RenggliMetric()
+    metric = RenggliMetric(**{"random_state": test_seed})
     assert metric.metric_name == "renggli"
     assert metric.inference_type == "features"
     assert metric.dataset_dependent is True
     assert metric.fit_metric(
-        dummy_features_random, dummy_labels, random_state=test_seed
+        dummy_model_head, dummy_features_random, dummy_labels
     ) == pytest.approx(1 / dummy_n_classes, rel=0.2)
 
 
-def test_parc_class_perfect_features(dummy_features_perfect, dummy_labels, test_seed):
+def test_parc_class_perfect_features(
+    dummy_model_head, dummy_features_perfect, dummy_labels, test_seed
+):
     """Test PARC metric class for perfect features"""
-    metric = PARCMetric()
+    metric = PARCMetric(
+        **{"random_state": test_seed, "feat_red_dim": None, "scale_features": False}
+    )
     assert metric.metric_name == "parc"
     assert metric.inference_type == "features"
     assert metric.dataset_dependent is True
     assert metric.fit_metric(
-        dummy_features_perfect, dummy_labels, random_sate=test_seed
+        dummy_model_head, dummy_features_perfect, dummy_labels
     ) == pytest.approx(100)
 
 
-def test_parc_class_random_features(dummy_features_random, dummy_labels, test_seed):
+def test_parc_class_random_features(
+    dummy_model_head, dummy_features_random, dummy_labels, test_seed
+):
     """Test PARC metric class for random features"""
-    metric = PARCMetric()
+    metric = PARCMetric(
+        **{"random_state": test_seed, "feat_red_dim": None, "scale_features": False}
+    )
     assert metric.metric_name == "parc"
     assert metric.inference_type == "features"
     assert metric.dataset_dependent is True
     assert metric.fit_metric(
-        dummy_features_random, dummy_labels, random_sate=test_seed
+        dummy_model_head, dummy_features_random, dummy_labels
     ) == pytest.approx(0.0, abs=0.2)
 
 
-def test_logme_class_perfect_features(dummy_features_perfect, dummy_labels):
+def test_logme_class_perfect_features(
+    dummy_model_head, dummy_features_perfect, dummy_labels
+):
     """Test the LogME metric class for perfect features"""
     metric = LogMEMetric()
     assert metric.metric_name == "LogME"
     assert metric.inference_type == "features"
     assert metric.dataset_dependent is True
-    assert metric.fit_metric(dummy_features_perfect, dummy_labels) > 0
+    assert metric.fit_metric(dummy_model_head, dummy_features_perfect, dummy_labels) > 0
 
 
-def test_logme_class_random_features(dummy_features_random, dummy_labels):
+def test_logme_class_random_features(
+    dummy_model_head, dummy_features_random, dummy_labels
+):
     """Test the LogME metric class for random features"""
     metric = LogMEMetric()
     assert metric.metric_name == "LogME"
     assert metric.inference_type == "features"
     assert metric.dataset_dependent is True
-    assert metric.fit_metric(dummy_features_random, dummy_labels) < 0
+    assert metric.fit_metric(dummy_model_head, dummy_features_random, dummy_labels) < 0
