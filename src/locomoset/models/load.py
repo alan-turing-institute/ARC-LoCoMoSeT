de1d186d7c4c079dc3870c9be5dfa19c7b79e858
Jack Roberts <jroberts@turing.ac.uk>
2023-10-06 21:29:47 +0100

    :construction: wip preprocessing for fine-tuning

--- src/locomoset/models/load.py
+++ src/locomoset/models/load.py
@@ -1,32 +1,55 @@
 """
 Helper functions for loading models and preprocessors.
 """
-from typing import Optional
-
+from datasets import Dataset
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 from transformers.image_processing_utils import BaseImageProcessor
 from transformers.modeling_utils import PreTrainedModel
 
 
-def get_model_and_processor(
-    model_name: str, num_labels: Optional[int] = None
-) -> tuple[PreTrainedModel, BaseImageProcessor]:
-    """Load a model and its corresponding preprocessor.
+def get_processor(model_name: str) -> BaseImageProcessor:
+    """Load a model's image preprocessor.
 
     Args:
-        model_name: Name or path to model and preprocessor to load.
-        num_labels: If set, Replace model head with a layer for this many labels. If
-            zero return the model without its classification head.
+        model_name: Name or path of model to load preprocessor for.
 
     Returns:
-        Model and preprocessor.
+        Image processor.
     """
-    if num_labels is not None:
-        model = AutoModelForImageClassification.from_pretrained(
-            model_name, num_labels=num_labels
-        )
-    else:
-        model = AutoModelForImageClassification.from_pretrained(model_name)
+    return AutoImageProcessor.from_pretrained(model_name)
 
-    processor = AutoImageProcessor.from_pretrained(model_name)
-    return model, processor
+
+def get_model_without_head(model_name: str) -> PreTrainedModel:
+    """Load a model with its classification head removed (or, more precisely, replaced
+    with torch.nn.Identity).
+
+    Args:
+        model_name: Name or path to model to load.
+
+    Returns:
+        Model without its classification head.
+    """
+    return AutoModelForImageClassification.from_pretrained(model_name, num_labels=0)
+
+
+def get_model_with_dataset_labels(model_name: str, dataset: Dataset) -> PreTrainedModel:
+    """Load a model with its classification head replaced with a new one with the
+    correct number of labels.
+
+    Args:
+        model_name: Name or path to model to load.
+        dataset: Dataset to use to get the number of classes and class index to label
+            mappings. Must contain a "label" feature that is an instance of
+            datasets.ClassLabel.
+
+    Returns:
+        Model with its classification head replaced with one with the correct number of
+        classes for the dataset, and the correct class index to label mappings.
+    """
+    return AutoModelForImageClassification.from_pretrained(
+        model_name,
+        num_labels=dataset.features["label"].num_classes,
+        id2label=dataset.features["label"]._int2str,
+        label2id=dataset.features["label"]._str2int,
+        ignore_mismatched_sizes=True,
+    )
