86ec2d41ffef3ad2f3890e07f7ca0dfc81f899a7
eddableheath <edable-heath@turing.ac.uk>
2023-10-19 10:55:02 +0100

    :dog2: updated baskerville slurm script

--- baskerville_scripts/metric_exp_slurm_template.sh
+++ baskerville_scripts/metric_exp_slurm_template.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=myarrayjob
+#SBATCH --account vjgo8416-locomoset
+#SBATCH --qos turing
+#SBATCH --job-name locomoset_metric_experiment
+#SBATCH --time 0-0:30:0
+#SBATCH --nodes 1
+#SBATCH --gpus 1
+#SBATCH --cpus-per-gpu 36
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --array=1-10
@@ -8,7 +14,11 @@
 CONFIGSPATH=/PATH/TO/CONFIGS
 
 # Load required modules here (pip etc.)
-module pip
+module load python/3.10
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install .
 
 # Run script
 python3 src/locomoset/run/run_metrics.py ${CONFIGSPATH}/config_${SLURM_ARRAY_TASK_ID}.yaml
