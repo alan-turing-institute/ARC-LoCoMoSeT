4c9addc638b3c649e98b431acb6d3e358cc01df3
eddableheath <edable-heath@turing.ac.uk>
2023-10-23 13:42:46 +0100

    updated pyproject.toml to change python version for baskerville

--- baskerville_scripts/MS2/conda_setup.sh
+++ baskerville_scripts/MS2/conda_setup.sh
@@ -1,4 +1,4 @@
-cd /bask/projects/v/vjgo8416-mod-sim-2
+cd /bask/projects/v/vjgo8416-locomoset/ARC-LoCoMoSeT
 module restore system
 module load bask-apps/test
 module load Miniconda3/4.10.3
@@ -7,7 +7,7 @@ eval "$(${EBROOTMINICONDA3}/bin/conda shell.bash hook)"
 
 # Define the path to your Conda environment (modify as appropriate)
 # N.B. this path will be created by the subsequent commands if it doesn't already exist
-CONDA_ENV_PATH="/bask/projects/v/vjgo8416-mod-sim-2/ms2env"
+CONDA_ENV_PATH="/bask/projects/v/vjgo8416-locomoset/ARC-LoCoMoSeT/locomosetenv"
 
 # Create the environment. Only required once.
 conda create --yes --prefix "${CONDA_ENV_PATH}"
