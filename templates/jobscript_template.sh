636f7c9a7ae58f79c783ad04dde67c5717ede0f5
eddableheath <edable-heath@turing.ac.uk>
2023-11-01 17:05:43 +0000

    :rock: updated jobscript template

--- templates/jobscript_template.sh
+++ templates/jobscript_template.sh
@@ -7,7 +7,7 @@
 #SBATCH --gpus {{ gpu_number }}
 #SBATCH --cpus-per-gpu {{ cpu_per_gpu }}
 #SBATCH --array=1-{{ array_number }}
-#SBATCH --output ./slurm_logs/locomoset_metric_experiment-%j.out
+#SBATCH --output ./slurm_logs/locomoset_{{ config_type }}_experiment-%j.out
 
 # Load required modules here (pip etc.)
 module purge
