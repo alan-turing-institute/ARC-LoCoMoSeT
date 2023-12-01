eaf6903dfe0339bf610efe11f0a3c77120cfe53f
Jack Roberts <jroberts@turing.ac.uk>
2023-12-01 09:51:36 +0000

    remove debug statements

--- src/locomoset/datasets/preprocess.py
+++ src/locomoset/datasets/preprocess.py
@@ -40,7 +40,6 @@ def preprocess(
         ]
         return sample
 
-    print("DEBUG set writer_batch_size to 100")
     processed_dataset = dataset.map(
         proc_sample,
         batched=False,
